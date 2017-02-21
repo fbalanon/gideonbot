3d4f2d49025458e1cc3be1e0990dbc025f50781a
Francis Balanon <fbalanon@gmail.com>
2017-02-20 20:48:02 -0800

    adding favorites

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,24 @@
-module.exports = function(bot) {
-    bot.hear(/hey buddy/, function(res) {
+module.exports = function(gideonbot) {
+    gideonbot.hear(/hey buddy/, function(res) {
         return res.send("yes sir?");
     });
-    bot.respond(/What's your favorite dance move?/, function(res) {
-        return res.send("The robot of course!!!");
-    });
+
+    gideonbot.respond(/what is your favorite (.*)/, function(msg) {
+        var fav;
+        fav = msg.match[1];
+        console.log(fav);
+        switch (fav) {
+            case "dance move":
+            return msg.reply("The Robot of Course!!");
+            break;
+        case "band":
+            return msg.reply("It's gotta be Daft Punk!");
+            break;
+        case "programming language":
+            return msg.reply("Javascript, of course!");
+            break;
+        default:
+            return msg.reply("I don't have a favorite " + fav + ". What's yours?");
+   }
+ });
 };
