98234151f4d7f27a644962a682cc5445e877e299
Francis Balanon <fbalanon@gmail.com>
2017-02-20 20:34:51 -0800

    adding dance move

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,7 @@ module.exports = function(bot) {
     bot.hear(/gideon/, function(res) {
         return res.send("yes sir?");
     });
+    bot.respond(/What's your favorite dance move?/, function(res) {
+        return res.send("The robot of course!!!");
+    });
 };
