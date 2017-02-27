19ca5f1d5310ad3e9151151d0c493f9f003da9af
Francis Balanon <fbalanon@gmail.com>
2017-02-26 16:47:09 -0800

    changed random mood to functions

--- scripts/app.js
+++ scripts/app.js
@@ -9,9 +9,15 @@ var classTopics = ["Class 00: Installfest","Class 01: Command line JavaScript",
 "Class 02: Data types & loops","Class 03: Conditionals & functions","Class 04: Scope & closures","Class 05: Slack bot lab","Class 06: Objects & JSON","Class 07: Intro to the DOM","Class 08: Intro to jQuery & templating","Class 09: Ajax & APIs","Class 10: Asynchronous JavaScript & callbacks","Class 11: Advanced APIs","Class 12: Feedr lab","Class 13: Prototypal inheritance","Class 14: The module pattern & this","Class 15: Intro to CRUD and Firebase","Class 16: Deploying your app","Class 17: TBD (instructor-student choice)","Class 18: Final project lab","Class 19: Final project presentations & graduation!"];
 var classSlides =["https://github.com/svodnik/sfjs6/blob/master/00-installfest/JS-SF-6-00-installfest-slides.pdf", "https://github.com/svodnik/sfjs6/blob/master/01-command-line-JS/JS-SF-6-01-command-line-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/02-data-types-loops/JS-SF-6-02-data-types-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/03-conditionals-functions/JS-SF-6-03-conditionals-functions-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/04-scope-closures/JS-SF-6-04-scope-closures-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/05-slackbot-lab/JS-SF-6-05-slackbot-lab-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/06-objects-json/JS-SF-6-06-objects-json-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/07-dom-intro/JS-SF-6-07-dom-intro-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/08-jquery-templating/JS-SF-6-08-jquery-templating-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/09-ajax-apis/JS-SF-6-09-ajax-apis-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/10-async-callbacks/JS-SF-6-10-async-callbacks-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/11-advanced-apis/JS-SF-6-11-advanced-apis-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/12-feedr-lab/JS-SF-6-12-feedr-lab-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/13-prototypal-inheritance/JS-SF-6-13-prototypal-inheritance-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/14-module-pattern-this/JS-SF-6-14-module-pattern-this-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/15-crud-firebase/JS-SF-6-15-crud-firebase-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/16-deploying-your-app/JS-SF-6-16-deploying-your-app-slides.pdf","https://github.com/svodnik/sfjs6/blob/master/17-instructor-student-choice/JS-SF-6-17-instructor-student-choice-slides.pdf"];
 
-var randomReset = reset[Math.floor(Math.random() * reset.length)];
-var randomMotivate = motivate[Math.floor(Math.random() * motivate.length)];
-var randomSports = sports[Math.floor(Math.random() * sports.length)];
+var randomReset = function() {
+    return reset[Math.floor((Math.random() * reset.length))];
+};
+var randomMotivate = function() { 
+    return motivate[Math.floor((Math.random() * motivate.length))];
+};
+var randomSports = function() {
+    return sports[Math.floor((Math.random() * sports.length))];
+};
 
 var topics = function(){
     var string = "\n";
@@ -73,13 +79,13 @@ module.exports = function(gideonbot) {
         console.log(mood);
         switch (mood) {
             case "tired":
-                return status.reply("listen to some music for motivation! \n" + randomMotivate);
+                return status.reply("listen to some music for motivation! \n" + randomMotivate());
                 break;
             case "blah":
-                return status.reply("enjoy! \n" + randomSports);
+                return status.reply("enjoy! \n" + randomSports());
                 break;
             case "going crazy":
-                return status.reply("take a break \n" + randomReset);
+                return status.reply("take a break \n" + randomReset());
                 break;
             case "done":
                 return status.reply("Congratulations!!! \n" + "https://youtu.be/pIOOwhmkoLo");
@@ -88,65 +94,22 @@ module.exports = function(gideonbot) {
                 return status.reply("Sorry, I can't help you, phone a friend!");
         }
     });
-/*
- gideonbot.respond(/what's due?/i, function(msg) {
-    if ((month === 2) && (day < 28)) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/05-slackbot-lab "  + "https://github.com/svodnik/sfjs6/tree/master/06-objects-json");
-    } else if ((month === 3) && (day < 7)) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/07-dom-intro " + "https://github.com/svodnik/sfjs6/tree/master/08-jquery-templating");
-    } else if ((month === 3) && (day < 14)) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/09-ajax-apis " + "https://github.com/svodnik/sfjs6/tree/master/10-async-callbacks");
-    } else if ((month === 3) && (day < 21)) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/11-advanced-apis " + "https://github.com/svodnik/sfjs6/tree/master/12-feedr-lab");
-    } else if ((month === 3) && (day < 28)) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/13-prototypal-inheritance " + "https://github.com/svodnik/sfjs6/tree/master/14-module-pattern-this");
-    }  else if (((month === 3) && (day <= 31)) || ((month === 4) && (day < 4))) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/15-crud-firebase " + "https://github.com/svodnik/sfjs6/tree/master/16-deploying-your-app");
-    } else {
-    return msg.reply("I believe it's all on you now, good luck!!!");
-    }
-   });
-*/
 
- gideonbot.respond(/what's due?/i, function(res) {
-    if ((month == 2) && (day < 28)) {
-        console.log(month + "-" + day);
-        return res.reply("https://github.com/svodnik/sfjs6/tree/master/05-slackbot-lab "  + "https://github.com/svodnik/sfjs6/tree/master/06-objects-json");
-    } else {
-    return res.reply("I believe it's all on you now, good luck!!!");
-    }
-});
-
-/*
+ gideonbot.respond(/what's due/i, function(msg) {
     if ((month === 2) && (day < 28)) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/05-slackbot-lab "  + "https://github.com/svodnik/sfjs6/tree/master/06-objects-json");
+        return msg.reply("https://github.com/svodnik/sfjs6/tree/master/05-slackbot-lab "  + "https://github.com/svodnik/sfjs6/tree/master/06-objects-json");
     } else if ((month === 3) && (day < 7)) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/07-dom-intro " + "https://github.com/svodnik/sfjs6/tree/master/08-jquery-templating");
+        return msg.reply("https://github.com/svodnik/sfjs6/tree/master/07-dom-intro " + "https://github.com/svodnik/sfjs6/tree/master/08-jquery-templating");
     } else if ((month === 3) && (day < 14)) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/09-ajax-apis " + "https://github.com/svodnik/sfjs6/tree/master/10-async-callbacks");
+        return msg.reply("https://github.com/svodnik/sfjs6/tree/master/09-ajax-apis " + "https://github.com/svodnik/sfjs6/tree/master/10-async-callbacks");
     } else if ((month === 3) && (day < 21)) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/11-advanced-apis " + "https://github.com/svodnik/sfjs6/tree/master/12-feedr-lab");
+        return msg.reply("https://github.com/svodnik/sfjs6/tree/master/11-advanced-apis " + "https://github.com/svodnik/sfjs6/tree/master/12-feedr-lab");
     } else if ((month === 3) && (day < 28)) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/13-prototypal-inheritance " + "https://github.com/svodnik/sfjs6/tree/master/14-module-pattern-this");
+        return msg.reply("https://github.com/svodnik/sfjs6/tree/master/13-prototypal-inheritance " + "https://github.com/svodnik/sfjs6/tree/master/14-module-pattern-this");
     }  else if (((month === 3) && (day <= 31)) || ((month === 4) && (day < 4))) {
-    return msg.reply("https://github.com/svodnik/sfjs6/tree/master/15-crud-firebase " + "https://github.com/svodnik/sfjs6/tree/master/16-deploying-your-app");
+        return msg.reply("https://github.com/svodnik/sfjs6/tree/master/15-crud-firebase " + "https://github.com/svodnik/sfjs6/tree/master/16-deploying-your-app");
     } else {
-    return msg.reply("I believe it's all on you now, good luck!!!");
+        return msg.reply("I believe it's all on you now, good luck!!!");
     }
-   });*/
-/*
-gideonbot.respond(/I'm (.*)/, function(res) {
-        var mood = res.match[1]
-        if (mood === "tired") {
-          return res.send("listen to some music for motivation! \n" + randomMotivate);
-        } else if (mood === "blah"){
-          return res.send("enjoy! \n" + randomSports);
-        } else if (mood === "going crazy"){
-          return res.send("take a break \n" + randomReset);
-        } else if (mood === "done"){
-          return res.send("Congratulations!!! \n" + "https://youtu.be/pIOOwhmkoLo");
-        } else {
-          return res.send("Sorry, I can't help you, phone a friend!");
-        }
-    });*/
+    });
 };
